7d15905ca46ac7e088f609524013981b941bbc47
shariful962 <shariful0411@gmail.com>
2024-11-30 19:10:51 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// toggle menu
+
+const navLinks = document.getElementById("navlinks")
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
